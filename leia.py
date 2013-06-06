13cdb4a184ab3ab6826039213203fc510c769fca
Emily Yeh <xemily.yeh@gmail.com>
2013-06-06 12:46:09 -0700

    sorted list

---
+++ leia.py
@@ -0,0 +1,54 @@
+import csv
+from operator import itemgetter
+import bisect
+
+cr = csv.reader(open("testdoc.csv", "r"))
+data = [(row[0], row[1], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
+data.sort(key = itemgetter(0))
+data2 = {}
+for row in cr:
+	data2[row[0]] = row[1:]
+#Data is sorted list
+#Data2 is dictionary of words : info
+
+allWords = [] #Full list of Synsets and found words (Synset, [words])
+val = []
+vadsd = []
+ar = []
+arsd = []
+
+
+def displayDB():
+#Display Words and Information
+	for x in data:
+		print x[0],':',x[1:]
+
+def addToPile(target):
+#Adds word to records
+	pass
+
+def searchWord(target):
+#Returns tuple with target word's information, None otherwise
+	for x in data:
+		if x[0] == target:
+			# addToPile(target)
+			return x
+	return None
+
+def fast_searchWord(target):
+	if target in data2:
+		return true
+
+def sortWords():
+#Returns sorted array (words first)
+	data = [(row[0], row[1], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
+	data.sort(key = itemgetter(0))
+
+	for r in data:
+		dict[r[0]] = r[1:]
+
+def sortSynsets():
+#Returns sorted array (synsets first)
+	data = [(row[1], row[0], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
+	data.sort()
+

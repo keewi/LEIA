113d75735626ed0868cf0292741cb245cfd1dcb8
Emily Yeh <xemily.yeh@gmail.com>
2013-06-06 13:27:38 -0700

    menu

--- leia.py
+++ leia.py
@@ -1,15 +1,11 @@
 import csv
 from operator import itemgetter
-import bisect
+import msvcrt
 
 cr = csv.reader(open("testdoc.csv", "r"))
 data = [(row[0], row[1], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
 data.sort(key = itemgetter(0))
-data2 = {}
-for row in data:
-	data2[row[0]] = row[1:]
 #Data is sorted list
-#Data2 is dictionary of words : info
 
 allWords = [] #Full list of Synsets and found words (Synset, [words])
 val = []
@@ -35,12 +31,6 @@ def searchWord(target):
 			return x
 	return None
 
-def fast_searchWord(target):
-#Returns tuple with target word's information, None otherwise
-	if target in data2:
-		return data2[target]
-	return None
-
 def sortWords():
 #Returns sorted array (words first)
 	data = [(row[0], row[1], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
@@ -51,5 +41,28 @@ def sortSynsets():
 	data = [(row[1], row[0], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
 	data.sort(key = itemgetter(0))
 
-print fast_searchWord('happy')
+print """MENU:
+1. Single Word Analysis
+2. Document Analysis
+3. Display Database
+4. Quit\n"""
+answer = raw_input("Please choose an option: ") 
+#Set as an option if you are only using one
 
+if answer == "1":
+	target = (raw_input("Enter word: ")).lower()
+	find = searchWord(target)
+	if find == None:
+		print "Not found."
+	else:
+		print "\nRESULTS: "
+		print "Synset:      ", find[1]
+		print "Valence:     ", find[2]
+		print "Valence(sd): ", find[3]
+		print "Arousal:     ", find[4]
+		print "Arousal(sd): ", find[5]
+
+elif answer == "2":
+	pass
+elif answer == "3":
+	displayDB()

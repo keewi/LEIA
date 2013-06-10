43a6f6528aade38b1487c1b46d300008be2937be
Emily Yeh <xemily.yeh@gmail.com>
2013-06-10 15:43:09 -0700

    added analyzing text (displaying saved data)

--- leia.py
+++ leia.py
@@ -7,7 +7,8 @@ data.sort(key = itemgetter(0))
 #Data is sorted list
 
 keys = []
-pile = {}
+pile = {} #{Synset : [[Vals], [Ars], [Words], freq]}
+aP = {} #{Synset : [Val, ]}
 
 def displayDB():
 #Display Words and Information
@@ -15,17 +16,18 @@ def displayDB():
 		print x[0],':',x[1:]
 
 def addToPile((word,synset,val,valsd,ar,arsd)):
-#Adds word to records
+#Adds word to records. NEED TO DO: ADD IN SD's!
 	if synset in keys:
 		(pile[synset][0]).append(val)
 		(pile[synset][1]).append(ar)
 		if word not in pile[synset][2]:
 			(pile[synset][2]).append(word)
-		# print pile[synset]
+		(pile[synset][3]) += 1
 		# print "New word added!"
 	else:
 		keys.append(synset)
-		pile[synset] = ([val], [ar], [word])
+		pile[synset] = [[val], [ar], [word], 1]
+		# print pile[synset]
 		# print "New synset added!"
 
 def searchWord(target):
@@ -33,19 +35,21 @@ def searchWord(target):
 	for x in data:
 		if x[0] == target:
 			addToPile(x)
-			# print x
 			return x
 	return None
 
-def sortWords():
-#Returns sorted array (words first)
-	data = [(row[0], row[1], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
-	data.sort(key = itemgetter(0))
+def analyzePile():
+#Analyzes pile, saves as aP
+	def avg(list):
+		return (sum(list)/len(list))
+	for synset in keys:
+		aP[synset] = [avg(pile[synset][0]), avg(pile[synset][1]), pile[synset][2], pile[synset][3]]
 
-def sortSynsets():
-#Returns sorted array (synsets first)
-	data = [(row[1], row[0], int(row[2]), int(row[3]), int(row[4]), int(row[5])) for row in cr]
-	data.sort(key = itemgetter(0))
+def displayAnalyzed():
+	print "Synset\tFreq.\tAvg.Valence\tAvg.Arousal\tWords"
+	for s in keys: #need to do: make this a nicer-looking table
+		print s, "\t", aP[s][3],"\t",aP[s][0],"\t\t",aP[s][1],"\t\t",aP[s][2]
+
 
 print """MENU:
 1. Single Word Analysis
@@ -74,7 +78,8 @@ elif answer == "2":
 	# print words
 	for s in words:
 		searchWord(s)
-	print pile
+	analyzePile()
+	displayAnalyzed()
 
 elif answer == "3":
 	displayDB()

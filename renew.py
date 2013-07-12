4ff18581c01178cf7449d72c52012f4bcedca5a1
Emily Yeh <xemily.yeh@gmail.com>
2013-07-12 11:42:31 -0700

    renew 1

--- renew.py
+++ renew.py
@@ -4,28 +4,29 @@ import wx
 from decimal import Decimal
 from operator import itemgetter
 
+d = open("db.csv","r")
+dr = csv.reader(d)
+data_dr = [(row[0],row[1],row[2],row[3],row[4],row[5]) for row in dr]
 
+d = open("db.csv","w")#call it newdb.csv to make a new one if revising code
+dw = csv.writer(d, lineterminator = '\n')
 
 def order():
+	print "ordering"
 	data_dr.sort(key = itemgetter(2))
 
-def searchWord(target):
-	for x in data_sr:
-		if x[0] == target:
-			return x
-	return None
-def run():
-	d = open("db.csv","r")
-	dr = csv.reader(d)
-	data_dr = [(row[0],row[1],row[2],row[3],row[4],row[5]) for row in dr]
 
-	d = open("db.csv","w")#call it newdb.csv to make a new one if revising code
-	dw = csv.writer(d, lineterminator = '\n')order()
+def run():
+	order()
 	try:
 		with open('anew.csv', 'r') as f:
 			sr = csv.reader(f)
 			data_sr = [(row[0], float(row[2]), float(row[3]), float(row[4]), float(row[5])) for row in sr]
-
+		def searchWord(target):
+					for x in data_sr:
+						if x[0] == target:
+							return x
+					return None
 		for word in data_dr:
 			f = searchWord(word[0])
 			if f != None:
@@ -35,10 +36,13 @@ def run():
 			else:
 				a = list(word)
 				dw.writerow(word)
-		msg = wx.MessageDialog(None, "Database sync complete.","Completed",wx.OK)
-		msg.ShowModal()
-		msg.Destroy()
+		# msg = wx.MessageDialog(None, "Database sync complete.","Completed",wx.OK)
+		# msg.ShowModal()
+		# msg.Destroy()
 	except IOError:
-		msg = wx.MessageDialog(None, "Unknown error: Unable to sync database.","ERROR",wx.OK|wx.ICON_ERROR)
-		msg.ShowModal()
-		msg.Destroy()
+		pass
+		# msg = wx.MessageDialog(None, "Unknown error: Unable to sync database.","ERROR",wx.OK|wx.ICON_ERROR)
+		# msg.ShowModal()
+		# msg.Destroy()
+
+run()
